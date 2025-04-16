793092b7ccd0d7c09fba2e177f378a5d6efc2385
Ridan Shrestha <ridanstha09@gmail.com>
2025-04-17 00:25:42 +0545

    renamed user table to users

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,12 +3,12 @@ create table department(
     department_name VARCHAR(30) NOT NULL
 );
 
-create table user(
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(50) NOT NULL,
-    user_email VARCHAR(225),
-    user_phone VARCHAR(15),
-    user_gender VARCHAR(15),
+create table users(
+    users_id INT AUTO_INCREMENT PRIMARY KEY,
+    users_name VARCHAR(50) NOT NULL,
+    users_email VARCHAR(225),
+    users_phone VARCHAR(15),
+    users_gender VARCHAR(15),
     role ENUM('doctor', 'patient', 'admin') NOT NULL
 );
 
@@ -21,16 +21,16 @@ create table doctor(
 
 create table doctor_user(
     doctor_id INT UNIQUE,
-    user_id INT UNIQUE,
-    PRIMARY KEY (doctor_id, user_id),
+    users_id INT UNIQUE,
+    PRIMARY KEY (doctor_id, users_id),
     FOREIGN KEY (doctor_id) REFERENCES doctor(doctor_id),
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    FOREIGN KEY (users_id) REFERENCES users(users_id)
 );
 
 create table patient(
     patient_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT UNIQUE,
-    FOREIGN KEY (user_id) REFERENCES user(user_id)
+    users_id INT UNIQUE,
+    FOREIGN KEY (users_id) REFERENCES users(users_id)
 );
 
 create table appointment(
