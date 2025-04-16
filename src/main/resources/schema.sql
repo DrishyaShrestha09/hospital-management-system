e1c9282449604f017610e178ee3ede95a86c85b8
Ridan Shrestha <ridanstha09@gmail.com>
2025-04-16 23:59:20 +0545

    fixed schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,23 +3,34 @@ create table department(
     department_name VARCHAR(30) NOT NULL
 );
 
+create table user(
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(50) NOT NULL,
+    user_email VARCHAR(225),
+    user_phone VARCHAR(15),
+    user_gender VARCHAR(15),
+    role ENUM('doctor', 'patient', 'admin') NOT NULL
+);
+
 create table doctor(
     doctor_id INT AUTO_INCREMENT PRIMARY KEY,
-    doctor_name VARCHAR(50) NOT NULL,
-    doctor_email VARCHAR(225),
-    doctor_phone VARCHAR(15),
-    doctor_gender VARCHAR(10),
     experience INT,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
+create table doctor_user(
+    doctor_id INT UNIQUE,
+    user_id INT UNIQUE,
+    PRIMARY KEY (doctor_id, user_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctor(doctor_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
 create table patient(
     patient_id INT AUTO_INCREMENT PRIMARY KEY,
-    patient_name VARCHAR(225) NOT NULL,
-    patient_gender VARCHAR(10),
-    patient_phone VARCHAR(15),
-    patient_email VARCHAR(225)
+    user_id INT UNIQUE,
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
 create table appointment(
