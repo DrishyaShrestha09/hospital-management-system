1d6cca76e072e56ddedc2a335e4dd9269838f9ec
Ridan Shrestha <ridanstha09@gmail.com>
2025-04-21 09:24:30 +0545

    Recreated Sql files(check todo and fix)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,46 +1,59 @@
+-- Department Table
 create table department(
     department_id INT AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
+-- User Table
+-- change profile form blob to string
 create table users(
-    users_id INT AUTO_INCREMENT PRIMARY KEY,
-    users_name VARCHAR(50) NOT NULL,
-    users_email VARCHAR(225),
-    users_phone VARCHAR(15),
-    users_gender VARCHAR(15),
-    role ENUM('doctor', 'patient', 'admin') NOT NULL
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(200) NOT NULL,
+    user_email VARCHAR(225) UNIQUE NOT NULL,
+    user_password VARCHAR(225) NOT NULL,
+    user_phone VARCHAR(15),
+    user_address VARCHAR(225),
+    user_gender VARCHAR(15),
+    role ENUM('doctor', 'patient', 'admin') NOT NULL,
+    profile BLOB
 );
 
+-- Doctor Table
 create table doctor(
     doctor_id INT AUTO_INCREMENT PRIMARY KEY,
     experience INT,
+    specialty VARCHAR(100),
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(department_id)
 );
 
+-- DoctorUser junction table
 create table doctor_user(
     doctor_id INT UNIQUE,
-    users_id INT UNIQUE,
-    PRIMARY KEY (doctor_id, users_id),
+    user_id INT UNIQUE,
+    PRIMARY KEY (doctor_id, user_id),
     FOREIGN KEY (doctor_id) REFERENCES doctor(doctor_id),
-    FOREIGN KEY (users_id) REFERENCES users(users_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
+-- Patient Table
 create table patient(
     patient_id INT AUTO_INCREMENT PRIMARY KEY,
-    users_id INT UNIQUE,
-    FOREIGN KEY (users_id) REFERENCES users(users_id)
+    user_id INT UNIQUE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
+-- Appointment Table
 create table appointment(
     appointment_id INT AUTO_INCREMENT PRIMARY KEY,
     appointment_date DATE NOT NULL,
+    cause VARCHAR(225),
     doctor_id INT,
     FOREIGN KEY (doctor_id) REFERENCES doctor(doctor_id)
 
 );
 
+-- Appointment patient junction table
 create table appointment_patient(
     appointment_id INT,
     patient_id INT,
